74ea6b4fa95cc3fd9662a1d2cc4e1c54d5f9f341
keith <skchiah@gmail.com>
2018-05-27 20:06:39 +0800

    tested edition

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
 
+// For heroku env use, in package.json - scripts > "test": "export NODE_ENV=test || SET NODE_ENV=test && mocha server/**/*.test.js",
+console.log("Enviroment: ", env);
+
 if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
